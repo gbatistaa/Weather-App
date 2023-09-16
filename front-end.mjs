9c125ceac6581bf2cfe4a620cd148f37836ef6ca
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2023-09-16 00:13:31 -0300

    front end

--- front-end.mjs
+++ front-end.mjs
@@ -1,15 +1,16 @@
-export const btn = document.getElementById('city-input');
+export const btn = document.getElementById('weather-button');
 export const icon = document.getElementById('weather-icon');
 export const city = document.getElementById('city-input');
 export const description = document.getElementById('weather-description');
 export const infos = document.getElementsByClassName('infos');
 export const infosData = document.getElementsByClassName('info-data');
 
-btn.addEventListener("focus", (e) => {
+city.addEventListener("focus", (e) => {
     e.preventDefault();
-    btn.style.outlineColor = ' #ff9900';
-    btn.style.outlineStyle = 'solid';
+    city.style.outlineColor = ' #ff9900';
+    city.style.outlineStyle = 'solid';
 })
-btn.addEventListener("blur", (e) => {
-    btn.style.outlineColor = '#2fafcf';
+city.addEventListener("blur", (e) => {
+    city.style.outlineColor = '#2fafcf';
 })
+city
