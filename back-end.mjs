8a7c5d408b686a598dec296ce5f7c65a33113c26
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2023-09-16 14:52:06 -0300

    estilização, estrutura e lógica prontas

--- back-end.mjs
+++ back-end.mjs
@@ -9,43 +9,49 @@ weather.responseType = 'json';
 
 // Defincição da função que realiza a requsição http:
 
-function weatherRequest () {
+function weatherRequest (e) {
+    if (e.type === "click" || (e.type === "keyup" && e.key === "Enter")) {
 
-    const cityValue = city.value;
-
-    geo.onload = () => {
-        if(geo.status < 300) {
-            const respGeo = geo.response[0];
-            weather.open("GET", `https://api.openweathermap.org/data/2.5/weather?lat=${respGeo.lat}&lon=${respGeo.lon}&appid=a5c989eb2a92a83339899d6a9fc8c0f5`)
-            weather.send() 
-        } else {
-            throw new Error(`Request Failed, error ${geo.status}`)
-        }
-    }
+        const cityValue = city.value;
     
-    geo.open("GET", `http://api.openweathermap.org/geo/1.0/direct?q=${cityValue}&appid=a5c989eb2a92a83339899d6a9fc8c0f5`);
-    geo.send();
-    
-    weather.onload = () => {
-        if (weather.status < 300) {
-            let index = 0
-            const respWeather = weather.response;
-            console.log(respWeather);
-            console.log(infos)
-            const main = respWeather.main
-            const data = [main.temp, main.feels_like, main.humidity, main.pressure];
-            for (const span in infosData) {
-                if (typeof infosData[span] == "object" && index < 4) {
-                    if (index < 2) infosData[span].innerHTML = `${(data[index] - 273).toFixed(1)} ºC`;
-                    else if (index === 2) infosData[span].innerHTML = `${data[index].toFixed(1)} %`;
-                    else infosData[span].innerHTML = data[index];
-                    index += 1
-                }
-            }
-        } else {
-            throw new Error(`Request Failed, error ${weather.status}`)
-        }
+        geo.onload = () => {
+            if(geo.status < 300) {
+                const respGeo = geo.response[0];
+                weather.open("GET", `https://api.openweathermap.org/data/2.5/weather?lat=${respGeo.lat}&lon=${respGeo.lon}&appid=a5c989eb2a92a83339899d6a9fc8c0f5`)
+                weather.send() 
+            } else {
+                throw new Error(`Request Failed, error ${geo.status}`)
+            };
+        };
+        
+        geo.open("GET", `http://api.openweathermap.org/geo/1.0/direct?q=${cityValue}&appid=a5c989eb2a92a83339899d6a9fc8c0f5`);
+        geo.send();
+        
+        weather.onload = () => {
+            if (weather.status < 300) {
+                let index = 0;
+                const respWeather = weather.response;
+                console.log(respWeather);
+                console.log(infos);
+                const main = respWeather.main;
+                const data = [main.temp, main.feels_like, main.humidity, main.pressure];
+                for (const span in infosData) {
+                    if (typeof infosData[span] == "object" && index < 4) {
+                        if (index < 2) infosData[span].innerHTML = `${(data[index] - 273).toFixed(1)} ºC`;
+                        else if (index === 2) infosData[span].innerHTML = `${data[index].toFixed(1)} %`;
+                        else infosData[span].innerHTML = `${data[index]} mmHg`;
+                        index += 1
+                    };
+                };
+                description.innerHTML = respWeather.weather[0].description;
+                const figure = respWeather.weather[0].icon;
+                icon.src = `https://openweathermap.org/img/wn/${figure}@2x.png`;
+            } else {
+                throw new Error(`Request Failed, error ${weather.status}`);
+            };
+        };
     }
-}
+};
 
 btn.addEventListener("click", weatherRequest)
+window.addEventListener("keyup", weatherRequest);
--- front-end.mjs
+++ front-end.mjs
@@ -4,13 +4,13 @@ export const city = document.getElementById('city-input');
 export const description = document.getElementById('weather-description');
 export const infos = document.getElementsByClassName('infos');
 export const infosData = document.getElementsByClassName('info-data');
+export const dataSection = document.getElementsByClassName('data-sec');
 
 city.addEventListener("focus", (e) => {
     e.preventDefault();
-    city.style.outlineColor = ' #ff9900';
+    city.style.outlineColor = '#ff9900';
     city.style.outlineStyle = 'solid';
 })
 city.addEventListener("blur", (e) => {
     city.style.outlineColor = '#2fafcf';
 })
-city
